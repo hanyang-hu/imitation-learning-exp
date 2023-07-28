fba1efdf5c0ca7584325a6228a5bde1766fe2c74
Hanyang Hu <63213964+KoHomerHu@users.noreply.github.com>
2023-07-29 06:38:56 +0800

    Fix bugs and allow save of model

--- BC.py
+++ BC.py
@@ -28,32 +28,35 @@ class DeepSetPolicyNet(torch.nn.Module):
     
 
 class BehaviorClone:
-    def __init__(self, state_dim, hidden_dim, action_dim, lr, device, focal_loss = False, gamma = 1.5):
+    def __init__(self, state_dim, hidden_dim, action_dim, lr, device, focal_loss = False, gamma = 2, soft = False):
         self.policy = DeepSetPolicyNet(state_dim, hidden_dim, action_dim).to(device)
         self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=lr)
         self.scheduler = lr_scheduler.LinearLR(self.optimizer, start_factor=1.0, end_factor=0.1, total_iters=100)
         self.device = device
         self.focal_loss = focal_loss
         self.gamma = gamma
+        self.soft = soft
 
     def learn(self, states, actions):
-        states = torch.tensor(states, dtype=torch.float).to(self.device)
-        actions = torch.tensor(actions, dtype=torch.int64).view(-1, 1).to(self.device)
+        states = states.to(self.device)
+        actions = actions.view(-1, 1).to(self.device)
         log_probs = torch.log(self.policy(states).gather(1, actions)) 
         if self.focal_loss:
             bc_loss = torch.mean(-log_probs * torch.pow(1 - torch.exp(log_probs), self.gamma))
         else:
             bc_loss = torch.mean(-log_probs) # maximum log-likelihood <=> minimum cross-entropy loss
-
         self.optimizer.zero_grad()
         bc_loss.backward()
         self.optimizer.step()
 
     def take_action(self, state):
-        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        state = torch.tensor(np.array([state]), dtype=torch.float).to(self.device)
         probs = self.policy(state)
-        action_dist = torch.distributions.Categorical(probs)
-        action = action_dist.sample()
+        if self.soft:
+            action_dist = torch.distributions.Categorical(probs)
+            action = action_dist.sample()
+        else:
+            action = torch.argmax(probs) # we are doing BC here, let's make hard decisions
         return action.item()
 
 
@@ -61,13 +64,14 @@ def test_agent(agent, env, n_episode):
     return_list = []
     for _ in range(n_episode):
         episode_return = 0
-        state = env.reset()
-        done = False
-        while not done:
+        state, _ = env.reset()
+        done = truncated = False
+        while not (done or truncated):
             action = agent.take_action(state)
-            next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, truncated, _ = env.step(action)
             state = next_state
             episode_return += reward
+            env.render()
         return_list.append(episode_return)
     return np.mean(return_list)
 
@@ -100,11 +104,11 @@ if __name__ == '__main__':
     lr = 1e-3
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     bc_agent = BehaviorClone(state_dim, hidden_dim, action_dim, lr, device)
-    n_iterations = 100
+    n_iterations = 10
     batch_size = 64
     test_returns = []
 
-    dataset = TransitionDataset('transition_data.pkl')
+    dataset = TransitionDataset('transition_data_mc.pkl')
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8)
 
     with tqdm(total=n_iterations, desc="Progress Bar") as pbar:
@@ -112,12 +116,14 @@ if __name__ == '__main__':
             for batch in dataloader:
                 expert_s, expert_a = batch['state'], batch['action']
                 bc_agent.learn(expert_s, expert_a)
-            current_return = test_agent(bc_agent, env, 5)
+            current_return = test_agent(bc_agent, env, 2)
             test_returns.append(current_return)
-            if (i + 1) % 5 == 0:
-                pbar.set_postfix({'return': '%.3f' % np.mean(test_returns[-5:])})
+            if (i + 1) % 2 == 0:
+                pbar.set_postfix({'return': '%.3f' % np.mean(test_returns[-2:])})
             pbar.update(1)
             bc_agent.scheduler.step()
+
+    torch.save(bc_agent.state_dict(), "bc_deep_set.pt")
 
     iteration_list = list(range(len(test_returns)))
     plt.plot(iteration_list, test_returns)
