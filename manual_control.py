b375e5234eb9ed037852c4ceb88f8c259dd388fc
Hanyang Hu <63213964+KoHomerHu@users.noreply.github.com>
2023-07-29 05:04:06 +0800

    Working version of manual_control.py

--- manual_control.py
+++ manual_control.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 import random
 import os
 import pickle
+from highway_env.envs.common.graphics import action_listener
 
 env = gym.make('highway-v0', render_mode = 'rgb_array')
 config = {
@@ -22,11 +23,20 @@ config = {
 env.configure(config)
 env.config["duration"] = 60
 
-filename = './data/transition_data.pkl'
+filename = './data/transition_data_mc.pkl'
 data = []
 
 if os.path.exists(filename):
     data = pickle.load(open(filename, 'rb'))
+
+current_action = 1
+ACTIONS_ALL = {
+        0: 'LANE_LEFT',
+        1: 'IDLE',
+        2: 'LANE_RIGHT',
+        3: 'FASTER',
+        4: 'SLOWER'
+    }
 
 i = 0
 while i < 100:
@@ -41,15 +51,18 @@ while i < 100:
 
     cnt = 0
     while not (done or truncated):
+        # print(state[0])
         next_state, reward, done, truncated, info = env.step(env.action_space.sample())
-        state_action_pair.append({'state': state, 'action': info['action'], 
+        current_action = action_listener()
+        state_action_pair.append({'state': state, 'action': current_action, 
                                   'reward': reward, 'next_state': next_state})
+        # print(ACTIONS_ALL[current_action])
         state = next_state
         env.render()
         cnt += 1
 
     print(cnt)
-    if cnt >= 52:
+    if cnt >= 45:
         print("This episode is added to the dataset!!!")
         data = data + state_action_pair
     else:
@@ -57,3 +70,5 @@ while i < 100:
         i -= 1
 
 pickle.dump(data, open(filename, 'wb'))
+
+print("Data is stored!!!")
